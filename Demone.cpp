af0aa4a92fb226c5362027b32570bf01b92283a8
Daniele Spina s285377 <danspi@live.it>
2022-03-10 17:03:51 +0100

    costruttori

--- Demone.cpp
+++ Demone.cpp
@@ -6,4 +6,18 @@ public:
     int stamina_recuperata;
     int turni_fragmenti;
     int fragmenti_recuperati;
+
+    Demone(int _stamina_necessaria,
+           int _turni_recupero_stamina,
+           int _stamina_recuperata,
+           int _turni_fragmenti,
+           int _fragmenti_recuperati)
+    {
+
+        stamina_necessaria = _stamina_necessaria;
+        turni_recupero_stamina = _turni_recupero_stamina;
+        stamina_recuperata = _stamina_recuperata;
+        turni_fragmenti = _turni_fragmenti;
+        fragmenti_recuperati = _fragmenti_recuperati;
+    }
 };
--- Pandora.cpp
+++ Pandora.cpp
@@ -4,4 +4,11 @@ public:
     int stamina;
     int max_stamina;
     int fragmenti;
+
+    Pandora(int _stamina, int _max_stamina, int _fragmenti)
+    {
+        stamina = _stamina;
+        max_stamina = _max_stamina;
+        fragmenti = _fragmenti;
+    }
 };
